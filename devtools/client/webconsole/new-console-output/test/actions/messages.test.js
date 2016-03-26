5d98e85d583bd6eda8fa4f963e70894bc8c2cf32
Lin Clark <lclark@mozilla.com>
2016-03-26 11:50:17 -0400

    add tests for action creators

---
+++ devtools/client/webconsole/new-console-output/test/actions/messages.test.js
@@ -0,0 +1,33 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const expect = require("expect");
+
+const actions = require("devtools/client/webconsole/new-console-output/actions/messages");
+const constants = require("devtools/client/webconsole/new-console-output/constants");
+
+describe("Message action creator", function() {
+  describe("messsageAdd()", function() {
+    it("creates expected action", function() {
+      let message = {};
+      let action = actions.messageAdd(message);
+      expect(action).toEqual({
+        type: constants.MESSAGE_ADD,
+        message: {}
+      });
+    });
+  });
+
+  describe("messsagesClear()", function() {
+    it("creates expected action", function() {
+      let action = actions.messagesClear();
+      expect(action).toEqual({
+        type: constants.MESSAGES_CLEAR
+      });
+    });
+  });
+});
