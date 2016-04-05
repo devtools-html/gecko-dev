c06d214ca1ff220114017ca7db82490bf8617c8a
Lin Clark <lclark@mozilla.com>
2016-04-05 04:37:50 -0400

    Switch to mochitest

--- devtools/client/webconsole/new-console-output/test/utils/head.js
+++ devtools/client/webconsole/new-console-output/test/utils/head.js
@@ -1,42 +1,78 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
-
 "use strict";
 
-// @TODO consolidate with other XPCShell head files. See #16
+var { utils: Cu } = Components;
 
-var { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+Cu.import("resource://testing-common/Assert.jsm");
+Cu.import("resource://gre/modules/Task.jsm");
+
 var { require } = Cu.import("resource://devtools/shared/Loader.jsm", {});
-
-var Services = require("Services");
+var { BrowserLoader } = Cu.import("resource://devtools/client/shared/browser-loader.js", {});
 var DevToolsUtils = require("devtools/shared/DevToolsUtils");
+
+const Services = require("Services");
+Services.scriptloader.loadSubScript("chrome://mochitests/content/chrome/devtools/shared/webconsole/test/common.js", this);
+
 DevToolsUtils.testing = true;
-DevToolsUtils.dumpn.wantLogging = true;
-DevToolsUtils.dumpv.wantVerbose = false;
-
-const { storeFactory } = require("devtools/client/webconsole/new-console-output/store");
-
-const testPackets = new Map();
-testPackets.set("console.log", {
-  "from": "server1.conn4.child1/consoleActor2",
-  "type": "consoleAPICall",
-  "message": {
-    "arguments": [
-      "foobar",
-      "test"
-    ],
-    "columnNumber": 1,
-    "counter": null,
-    "filename": "file:///test.html",
-    "functionName": "",
-    "groupName": "",
-    "level": "log",
-    "lineNumber": 1,
-    "private": false,
-    "styles": [],
-    "timeStamp": 1455064271115,
-    "timer": null,
-    "workerType": "none",
-    "category": "webdev"
-  }
+var { require: browserRequire } = BrowserLoader({
+  baseURI: "resource://devtools/client/webconsole/",
+  window: this
 });
+
+let ReactDOM = browserRequire("devtools/client/shared/vendor/react-dom");
+let React = browserRequire("devtools/client/shared/vendor/react");
+var TestUtils = React.addons.TestUtils;
+
+function* getPacket(command, type = "evaluationResult") {
+  try {
+    // Attach the console to the tab.
+    let state = yield new Promise(function(resolve) {
+      attachConsoleToTab(["ConsoleAPI"], (state) => resolve(state));
+    });
+
+    // Run the command and get the packet.
+    let packet;
+    switch (type) {
+      case "consoleAPICall":
+        packet = yield new Promise((resolve) => {
+          function onConsoleApiCall(type, packet) {
+            state.dbgClient.removeListener("consoleAPICall", onConsoleApiCall);
+            resolve(packet)
+          };
+          state.dbgClient.addListener("consoleAPICall", onConsoleApiCall)
+          eval(`top.${command}`);
+        });
+        break;
+      case "evaluate":
+        // @TODO support JavaScriptEvalOutput
+        // let evaluated =
+        //   new Promise(resolve => state.client.evaluateJSAsync("top.console.log(\"bitty\")", resolve));
+        // let packet = yield evaluated;
+        break;
+    }
+
+    closeDebugger(state);
+    return packet;
+  } catch (e) {
+    ok(false, "Got an error: " + DevToolsUtils.safeErrorString(e));
+  }
+}
+
+function renderComponent(component, props) {
+  const el = React.createElement(component, props, {});
+  // By default, renderIntoDocument() won't work for stateless components, but
+  // it will work if the stateless component is wrapped in a stateful one.
+  // See https://github.com/facebook/react/issues/4839
+  const wrappedEl = React.DOM.span({}, [el]);
+  const renderedComponent = TestUtils.renderIntoDocument(wrappedEl);
+  return ReactDOM.findDOMNode(renderedComponent).children[0];
+}
+
+function cleanActualHTML(htmlString) {
+  return htmlString.replace(/ data-reactid=\".*?\"/g, "");
+}
+
+function cleanExpectedHTML(htmlString) {
+  return htmlString.replace(/(?:\r\n|\r|\n)\s*/g, "");
+}
