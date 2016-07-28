e7122bca742989b8aeb8ddae1e7cfc5c484a54db
Lin Clark <lclark@mozilla.com>
2016-07-28 14:34:59 -0400

    Add test for FilterButton

---
+++ devtools/client/webconsole/new-console-output/test/requireHelper.js
@@ -0,0 +1,20 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+const requireHacker = require("require-hacker");
+
+module.exports = () => {
+  try {
+    requireHacker.global_hook("default", path => {
+      switch (path) {
+        case "react-dom/server":
+          return `const React = require('react-dev'); module.exports = React`;
+        case "react-addons-test-utils":
+          return `const React = require('react-dev'); module.exports = React.addons.TestUtils`;
+      }
+    });
+  } catch (e) {
+    // Do nothing. This means the hook is already registered.
+  }
+};
