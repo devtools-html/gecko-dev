1200a76971bc55441debebab9e13c3e889cbf4d8
Lin Clark <lclark@mozilla.com>
2016-08-15 12:52:19 -0400

    Update pageError stubs

--- devtools/client/webconsole/new-console-output/test/fixtures/stubs/pageError.js
+++ devtools/client/webconsole/new-console-output/test/fixtures/stubs/pageError.js
@@ -13,7 +13,7 @@ const { ConsoleMessage } = require("devtools/client/webconsole/new-console-outpu
 
 let stubConsoleMessages = new Map();
 
-stubConsoleMessages.set("key", new ConsoleMessage({
+stubConsoleMessages.set("ReferenceError: asdf is not defined", new ConsoleMessage({
 	"id": "1",
 	"allowRepeating": true,
 	"source": "javascript",
@@ -27,31 +27,19 @@ stubConsoleMessages.set("key", new ConsoleMessage({
 	"frame": null
 }));
 
-stubConsoleMessages.set("key", new ConsoleMessage({
+stubConsoleMessages.set("ReferenceError: foobar is not defined", new ConsoleMessage({
 	"id": "1",
 	"allowRepeating": true,
 	"source": "javascript",
 	"type": "log",
 	"level": "error",
-	"messageText": "1471047764458\tToolkit.GMP\tERROR\tGMPInstallManager.simpleCheckAndInstall Could not check for addons: Error: got node name: html, expected: updates (resource://gre/modules/addons/ProductAddonChecker.jsm:153:11) JS Stack trace: parseXML@ProductAddonChecker.jsm:153:11 < Async*ProductAddonChecker.getProductAddonList@ProductAddonChecker.jsm:320:12 < GMPInstallManager.prototype.checkForAddons@GMPInstallManager.jsm:107:5 < GMPInstallManager.prototype.simpleCheckAndInstall<@GMPInstallManager.jsm:204:29",
+	"messageText": "ReferenceError: foobar is not defined",
 	"parameters": null,
 	"repeat": 1,
-	"repeatId": "{\"id\":null,\"allowRepeating\":true,\"source\":\"javascript\",\"type\":\"log\",\"level\":\"error\",\"messageText\":\"1471047764458\\tToolkit.GMP\\tERROR\\tGMPInstallManager.simpleCheckAndInstall Could not check for addons: Error: got node name: html, expected: updates (resource://gre/modules/addons/ProductAddonChecker.jsm:153:11) JS Stack trace: parseXML@ProductAddonChecker.jsm:153:11 < Async*ProductAddonChecker.getProductAddonList@ProductAddonChecker.jsm:320:12 < GMPInstallManager.prototype.checkForAddons@GMPInstallManager.jsm:107:5 < GMPInstallManager.prototype.simpleCheckAndInstall<@GMPInstallManager.jsm:204:29\",\"parameters\":null,\"repeatId\":null,\"stacktrace\":null,\"frame\":null}",
+	"repeatId": "{\"id\":null,\"allowRepeating\":true,\"source\":\"javascript\",\"type\":\"log\",\"level\":\"error\",\"messageText\":\"ReferenceError: foobar is not defined\",\"parameters\":null,\"repeatId\":null,\"stacktrace\":null,\"frame\":null}",
 	"stacktrace": null,
 	"frame": null
 }));
 
-
-// Temporarily hardcode these
-stubConsoleMessages.set("ReferenceError", new ConsoleMessage({
-  allowRepeating: true,
-  source: MESSAGE_SOURCE.JAVASCRIPT,
-  type: MESSAGE_TYPE.LOG,
-  level: MESSAGE_LEVEL.ERROR,
-  messageText: "ReferenceError: asdf is not defined",
-  parameters: null,
-  repeat: 1,
-  repeatId: null,
-}));
 
 module.exports = stubConsoleMessages
