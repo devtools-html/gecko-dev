1b50d2d63f5efd77f63c2dd59dee45308d1c28e0
Lin Clark <lclark@mozilla.com>
2016-09-26 09:37:07 -0400

    Nits

--- devtools/client/webconsole/new-console-output/reducers/index.js
+++ devtools/client/webconsole/new-console-output/reducers/index.js
@@ -9,11 +9,12 @@ const { filters } = require("./filters");
 const { messages } = require("./messages");
 const { prefs } = require("./prefs");
 const { ui } = require("./ui");
+const constants = require("devtools/client/webconsole/new-console-output/constants");
 
 function enableBatching(reducer) {
   return function batchingReducer(state, action) {
     switch (action.type) {
-      case "BATCH_ACTIONS":
+      case constants.BATCH_ACTIONS:
         return action.actions.reduce(batchingReducer, state);
       default:
         return reducer(state, action);
