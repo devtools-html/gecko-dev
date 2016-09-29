5c7a50cd0753c216c140313295b388be11d1c977
Lin Clark <lclark@mozilla.com>
2016-09-29 11:59:30 -0400

    Convert page error.

--- devtools/client/webconsole/new-console-output/components/message.js
+++ devtools/client/webconsole/new-console-output/components/message.js
@@ -12,16 +12,29 @@ const {
   DOM: dom,
   PropTypes
 } = require("devtools/client/shared/vendor/react");
+const actions = require("devtools/client/webconsole/new-console-output/actions/index");
+const CollapseButton = createFactory(require("devtools/client/webconsole/new-console-output/components/collapse-button").CollapseButton);
 const MessageIcon = createFactory(require("devtools/client/webconsole/new-console-output/components/message-icon").MessageIcon);
+const MessageRepeat = createFactory(require("devtools/client/webconsole/new-console-output/components/message-repeat").MessageRepeat);
+const FrameView = createFactory(require("devtools/client/shared/components/frame"));
+const StackTrace = createFactory(require("devtools/client/shared/components/stack-trace"));
 
 Message.displayName = "Message";
 
 Message.propTypes = {
+  open: PropTypes.bool,
   source: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   level: PropTypes.string.isRequired,
   topLevelClasses: PropTypes.array,
   messageBody: PropTypes.any.isRequired,
+  repeat: PropTypes.any,
+  frame: PropTypes.any,
+  attachment: PropTypes.any,
+  stacktrace: PropTypes.any,
+  messageId: PropTypes.string,
+  onViewSourceInDebugger: PropTypes.func,
+  sourceMapService: PropTypes.any,
 };
 
 Message.defaultProps = {
@@ -30,30 +43,87 @@ Message.defaultProps = {
 
 function Message(props) {
   const {
+    messageId,
+    open,
     source,
     type,
     level,
     topLevelClasses: classes,
     messageBody,
+    frame,
+    stacktrace,
+    onViewSourceInDebugger,
+    sourceMapService,
+    dispatch,
   } = props;
-
-  const icon = MessageIcon({level});
 
   classes.push("message");
   classes.push(source);
   classes.push(type);
   classes.push(level);
+  if (open) {
+    classes.push("open");
+  }
+
+  const icon = MessageIcon({level});
+
+  // Figure out if there is an expandable part to the message.
+  let attachment = null;
+  if (props.attachment) {
+    attachment = props.attachment;
+  } else if (stacktrace) {
+    if (open) {
+      attachment = dom.div({ className: "stacktrace devtools-monospace" },
+        StackTrace({
+          stacktrace: stacktrace,
+          onViewSourceInDebugger: onViewSourceInDebugger
+        })
+      );
+    }
+  }
+
+  // If there is an expandable part, make it collapsible.
+  let collapse = null;
+  if (attachment) {
+    collapse = CollapseButton({
+      open,
+      onClick: function () {
+        if (open) {
+          dispatch(actions.messageClose(messageId));
+        } else {
+          dispatch(actions.messageOpen(messageId));
+        }
+      },
+    });
+  }
+
+  const repeat = props.repeat ? MessageRepeat({repeat: props.repeat}) : null;
+
+  // Configure the location.
+  const shouldRenderFrame = frame && frame.source !== "debugger eval code";
+  const location = dom.span({ className: "message-location devtools-monospace" },
+    shouldRenderFrame ? FrameView({
+      frame,
+      onClick: onViewSourceInDebugger,
+      showEmptyPathAsHost: true,
+      sourceMapService
+    }) : null
+  );
 
   return dom.div({ className: classes.join(" ") },
     // @TODO add timestamp
     // @TODO add indent if necessary
     icon,
+    collapse,
     dom.span({ className: "message-body-wrapper" },
       dom.span({ className: "message-flex-body" },
         dom.span({ className: "message-body devtools-monospace" },
           messageBody
-        )
-      )
+        ),
+        repeat,
+        location
+      ),
+      attachment
     )
   );
 }
